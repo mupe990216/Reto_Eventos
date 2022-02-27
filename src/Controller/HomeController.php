4b23d45933ef2b31f791b1b4b71d61f7660bf571
mupe990216 <emunozp1400@alumno.ipn.mx>
2022-02-26 21:36:57 -0600

    Twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,12 +9,30 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/",name="pagina_principal")
      */
     public function homePage(){
         $number = random_int(0,100);
         return new Response(
-            "Hola Mundo Con Symfony UwUr ".$number
+            "Hola Mundo Con Symfony UwUr ".$number.
+            "<br><button><a href='./Elias'>Saludar</button>"
         );
+    }
+
+    /**
+     * @Route("/{nombre}",name="saludador")
+     */
+    public function saludar($nombre){
+        if($nombre == "Elias"){
+            return new Response(
+                "<h1> Hola ".$nombre." UwU</h1>".
+                "<br><button><a href='/'>Regresar</button>"
+            );
+        }else{
+            return new Response(
+                "Usuario No autorizado "."<br><button><a href='/'>Regresar</button>"
+            );
+        }
+
     }
 }
